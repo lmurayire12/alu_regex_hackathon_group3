4df80afd2314e52cd83f7b24206e44d515336e97
Hugorkevin23 <125446773+Hugorkevin23@users.noreply.github.com>
2024-02-13 21:12:01 +0200

    bug fix

--- expressions.js
+++ expressions.js
@@ -1,8 +1,7 @@
 //Expression to extract Movie Titles
 const movieRegex = /^Title \((?:\d{4})\)$/;
 
-//Expression to extract Song lyrics
-const songLyricRegex = /^(?:\[Verse \d+\]) (.+)$/;
+//Expression to extract Song lyrics;
 
 //Expression to extract the date
 const dateRegex = /^(\d{2})-(\w{3})-(\d{4})$/;
