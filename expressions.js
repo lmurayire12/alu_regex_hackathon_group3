d4110d8e846a18b55be011d4d97b38998855af49
lmurayire12 <l.murayire@alustudent.com>
2024-02-13 03:42:20 -0800

    expressions

--- expressions.js
+++ expressions.js
@@ -1,2 +1,11 @@
+//Expression to extract Movie Titles
+const movieRegex = /^Title \((?:\d{4})\)$/;
+
+//Expression to extract Song lyrics
+const songLyricRegex = /^(?:\[Verse \d+\]) (.+)$/;
+
+//Expression to extract the date
 const dateRegex = /^(\d{2})-(\w{3})-(\d{4})$/;
+
+//Expression to extract ISBN numbers
 const isbnRegex = /^ISBN (\d{3})-(\d)-(\d{3})-(\d{5})-(\d)$/;
