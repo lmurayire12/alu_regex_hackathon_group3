49cfd71bb9acfdf3dd24c89261097b84dacf7a6c
ikalisa1 <ikalisa1@alustudent.com>
2024-02-13 03:33:26 -0800

    expressions

--- expressions.js
+++ expressions.js
@@ -1,3 +1,2 @@
-const movieRegex = /^Title \((?:\d{4})\)$/;
-const songLyricRegex = /^(?:\[Verse \d+\]) (.+)$/;
-
+const dateRegex = /^(\d{2})-(\w{3})-(\d{4})$/;
+const isbnRegex = /^ISBN (\d{3})-(\d)-(\d{3})-(\d{5})-(\d)$/;
